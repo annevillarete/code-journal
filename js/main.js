c939a332d5fedf27def7a98a3f2c9f0bc7d89fa3
Anne Villarete <annekamillev@gmail.com>
2021-02-19 21:05:51 -0800

    Completed Issue 3 Updated Profile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
 var $avatarUrl = document.querySelector('#avatar-url');
 var $avatarPhoto = document.querySelector('.avatar-photo');
-var $form = document.querySelector('#edit-profile');
+var $form = document.querySelector('#save-profile');
 var $dataEdit = document.querySelector('#edit-profile-view');
 var $profileView = document.querySelector('#profile-view');
+var $imageContainer = document.querySelector('#image-container');
 
 $avatarUrl.addEventListener('input', function (event) {
+  $avatarPhoto.setAttribute('class', 'container view');
   $avatarPhoto.setAttribute('src', event.target.value);
+  $imageContainer.setAttribute('class', 'hidden');
 });
+
+function viewSwapping(dataview) {
+
+  if (dataview === 'edit-profile') {
+    $dataEdit.className = 'container view';
+    document.getElementById('username').setAttribute('value', data.profile.username);
+    document.getElementById('fullName').setAttribute('value', data.profile.fullName);
+    document.getElementById('bio').setAttribute('value', data.profile.bio);
+    document.getElementById('location').setAttribute('value', data.profile.location);
+    $avatarUrl.setAttribute('value', data.profile.avatarUrl);
+    $avatarPhoto.setAttribute('src', data.profile.avatarUrl);
+    $avatarPhoto.setAttribute('class', 'container view');
+    $profileView.className = 'container hidden';
+    data.view = dataview;
+    $form.reset();
+
+  } else if (dataview === 'profile') {
+    renderProfile();
+    $profileView.className = 'container view';
+    data.view = dataview;
+    $dataEdit.className = 'container hidden';
+    $form.reset();
+  }
+}
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -17,23 +44,38 @@ $form.addEventListener('submit', function (event) {
   data.profile.bio = $form.elements.bio.value;
   $avatarPhoto.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
+
+  var $editProfileForm = document.querySelector('.editProfileContainer');
+
+  if ($editProfileForm !== null) {
+    $editProfileForm.remove();
+  }
+
   viewSwapping('profile');
 });
 
 function renderProfile() {
+
+  var containerdiv = document.createElement('div');
+  containerdiv.className = 'editProfileContainer';
+
+  var $form2 = document.createElement('form');
+  $form2.setAttribute('id', 'edit-profile');
+
   var $colFormgroup = document.createElement('div');
   $colFormgroup.setAttribute('class', 'col form-group');
+  $form2.appendChild($colFormgroup);
 
   var $py3ColSm = document.createElement('div');
   $py3ColSm.setAttribute('class', 'py-3 col-sm');
-  $profileView.appendChild($py3ColSm);
+  containerdiv.appendChild($py3ColSm);
 
   var $viewProfile = document.createElement('h3');
   $viewProfile.setAttribute('class', 'header row');
   $viewProfile.textContent = 'View Profile';
   $py3ColSm.appendChild($viewProfile);
 
-  var $fullName = document.createElement('h5');
+  var $fullName = document.createElement('h4');
   $fullName.setAttribute('class', 'fas fa-id-card');
   $fullName.textContent = data.profile.fullName;
   $py3ColSm.appendChild($fullName);
@@ -64,7 +106,7 @@ function renderProfile() {
   $colSmFormgroup2.appendChild($usernamelabel);
 
   var $username = document.createElement('div');
-  $username.setAttribute('class', 'col form-group');
+  $username.setAttribute('class', 'col form-group ');
   $username.textContent = data.profile.username;
   $colSmFormgroup2.appendChild($username);
 
@@ -86,7 +128,7 @@ function renderProfile() {
   $colSm.appendChild($colFormgroup4);
 
   var $bioIcon = document.createElement('i');
-  $bioIcon.setAttribute('class', 'fabashs fa-comment form-group');
+  $bioIcon.setAttribute('class', 'fas fa-comment form-group ');
   $colFormgroup4.appendChild($bioIcon);
 
   var $bio = document.createElement('div');
@@ -95,60 +137,54 @@ function renderProfile() {
   $colFormgroup4.appendChild($bio);
 
   var $colFormgroup5 = document.createElement('div');
-  $colFormgroup5.setAttribute('class', 'row col-sm form-group');
+  $colFormgroup5.setAttribute('class', 'row col-sm form-group flex-end');
   $colSm.appendChild($colFormgroup5);
-
-  var $edit = document.createElement('div');
-  $edit.setAttribute('class', 'col-sm form-group flex-end');
-  $colFormgroup5.appendChild($edit);
 
   var $editButton = document.createElement('button');
   $editButton.setAttribute('class', 'save-btn btn save-btn:hover save-btn');
   $editButton.setAttribute('id', 'edit-button');
+  $editButton.setAttribute('a', '');
   $editButton.setAttribute('href', '#');
-  $editButton.setAttribute('data-view', 'edit-profile');
+  $editButton.setAttribute('dataview', 'edit-profile');
+  $editButton.setAttribute('type', 'button');
   $editButton.textContent = 'EDIT';
-  $edit.appendChild($editButton);
+  $colFormgroup5.appendChild($editButton);
 
-  $profileView.appendChild($colFormgroup);
+  containerdiv.appendChild($form2);
+  $profileView.appendChild(containerdiv);
 
-  // $editButton.addEventListener('click', function (event) {
-  //   console.log($editButton)
-  //   viewSwapping('edit-profile');
-  // });
+  $editButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    viewSwapping('edit-profile');
+  });
+
 }
 
-function viewSwapping(dataview) {
+var $profileButton = document.querySelector('#navbar-profile > div > a:nth-child(1)');
 
-  if (dataview === 'edit-profile') {
-    renderProfile();
-    $dataEdit.className = 'container view';
-    data.view = dataview;
-    $form.reset();
+$profileButton.addEventListener('click', function (event) {
+  var $editProfileForm = document.querySelector('.editProfileContainer');
+  if ($editProfileForm !== null) {
+    $editProfileForm.remove();
 
-  } else if (dataview === 'profile') {
-    renderProfile();
-    $profileView.className = 'container view';
-    data.view = dataview;
-    $dataEdit.remove();
-    $form.reset();
   }
-}
 
-document.addEventListener('DOMContentLoaded', function (event) {
   if (data.profile.username === '') {
     viewSwapping('edit-profile');
-  } else if (data.profile.username) {
+  } else {
     viewSwapping('profile');
   }
 
 });
 
-// var $profileViewEditbutton = document.querySelector('#edit');
-// $profileViewEditbutton.addEventListener('click', function (event) {
-//   viewSwapping('edit-profile')
+document.addEventListener('DOMContentLoaded', function (event) {
+  if (data.profile.username === '') {
+    viewSwapping('edit-profile');
 
-document.addEventListener('click', function (event) {
-  event.preventDefault();
+  } else if (data.profile.username) {
+    viewSwapping('profile');
+    $imageContainer.setAttribute('class', 'hidden');
+
+  }
 
 });
